a4821b7372bb04ec2cd150921cd05cd6b5609b7f
Temaalex <alekseev_artem99991@mail.ru>
2025-08-17 18:26:32 +0300

    correct geo

--- src/Pages/PageOne.js
+++ src/Pages/PageOne.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from 'react';  
-import { useNavigate } from "react-router-dom"
+//import { useNavigate } from "react-router-dom"
 
 function PageOne() {  
 
-  let navigate = useNavigate();
-  const [location, setLocation] = useState({ latitude: null, longitude: null });  
+  //let navigate = useNavigate();
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });  
   useEffect(() => {  
   const getLocation = () => {  
   if (navigator.geolocation) {  
     navigator.geolocation.getCurrentPosition((position) => {  
-    setLocation({ latitude: position.coords.latitude.toFixed(5), longitude: position.coords.longitude.toFixed(5)});  
+    setLocation({ latitude: position.coords.latitude.toFixed(4), longitude: position.coords.longitude.toFixed(4)});  
   }, (error) => {  
     console.error("Error getting location:", error);  
   } );  
@@ -17,28 +17,24 @@ function PageOne() {
     console.error("Geolocation is not supported by this browser.");  
   }  
   };
-  getLocation();
-  //мы не на месте
-  //let x = 64.561142
-  //let y = 39.816177
-  //мы на месте
-  //let x = 64.561005
-  //let y = 39.816380
+  getLocation()
    },);  
   return (  
     <div>  
-    {location.latitude == 64.56114 && location.longitude == 39.81655 ? (  
+    {location.latitude == 64.5608 && location.longitude == 39.8139 ? (  
       <div>  
         <p>Latitude: {location.latitude}</p>  
         <p>Longitude: {location.longitude}</p>
         <p>Мы на месте</p>
+        <button onClick={() => setLocation(location.latitude,location.longitude)}>Нажми</button>  
       </div>  
       ) : (  
-    <p>
+    <div>
       <p>Latitude: {location.latitude}</p>  
       <p>Longitude: {location.longitude}</p>
       <p>Мы не на месте</p>
-      </p>  
+      <button onClick={() => setLocation(location.latitude,location.longitude)}>Нажми</button>  
+      </div>  
     )}  
     </div>  
   );  

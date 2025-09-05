ab5ce28be71cfb1ba49cf634b131cc06ba7f40cf
Temaalex <alekseev_artem99991@mail.ru>
2025-09-05 16:14:50 +0300

    correct geo

--- src/Pages/PageOne.js
+++ src/Pages/PageOne.js
@@ -1,47 +1,47 @@
-import { useState, useEffect } from 'react';  
+import { useState, useEffect } from 'react';
 //import { useNavigate } from "react-router-dom"
 
-function PageOne() {  
+function PageOne() {
 
   //let navigate = useNavigate();
-  const [location, setLocation] = useState({ latitude: 0, longitude: 0 }); 
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
   const geoOptoins = {
     enableHighAccuracy: true,
     maximumAge: 0,
     timeout: 3000,
-  } 
-  useEffect(() => {  
-  const getLocation = () => {  
-  if (navigator.geolocation) {  
-    navigator.geolocation.getCurrentPosition()((position) => {  
-    setLocation({ latitude: position.coords.latitude, longitude: position.coords.longitude});  
-  }, (error) => {  
-    console.error("Error getting location:", error);  
-  }, geoOptoins );  
-  } else {  
-    console.error("Geolocation is not supported by this browser.");  
-  }  
-  };
-  getLocation()
-   },);  
-  return (  
-    <div>  
-    {location.latitude == 64.5608 && location.longitude == 39.8139 ? (  
-      <div>  
-        <p>Latitude: {location.latitude}</p>  
-        <p>Longitude: {location.longitude}</p>
-        <p>Мы на месте</p>
-        <button onClick={() => setLocation(location.latitude,location.longitude)}>Нажми</button>  
-      </div>  
-      ) : (  
+  }
+  useEffect(() => {
+    const getLocation = () => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          setLocation({ latitude: position.coords.latitude, longitude: position.coords.longitude });
+        }, (error) => {
+          console.error("Error getting location:", error);
+        }, geoOptoins);
+      } else {
+        console.error("Geolocation is not supported by this browser.");
+      }
+    };
+    getLocation()
+  },);
+  return (
     <div>
-      <p>Latitude: {location.latitude}</p>  
-      <p>Longitude: {location.longitude}</p>
-      <p>Мы не на месте</p>
-      <button onClick={() => setLocation(location.latitude,location.longitude)}>Нажми</button>  
-    </div>  
-    )}  
-    </div>  
-  );  
- }  
+      {location.latitude == 64.5608 && location.longitude == 39.8139 ? (
+        <div>
+          <p>Latitude: {location.latitude}</p>
+          <p>Longitude: {location.longitude}</p>
+          <p>Мы на месте</p>
+          <button onClick={() => setLocation(location.latitude, location.longitude)}>Нажми</button>
+        </div>
+      ) : (
+        <div>
+          <p>Latitude: {location.latitude}</p>
+          <p>Longitude: {location.longitude}</p>
+          <p>Мы не на месте</p>
+          <button onClick={() => setLocation(location.latitude, location.longitude)}>Нажми</button>
+        </div>
+      )}
+    </div>
+  );
+}
 export default PageOne;  
